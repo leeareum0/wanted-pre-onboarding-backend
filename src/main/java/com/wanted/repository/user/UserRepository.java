ab84813bc6195c13d59b6516bd443496dcce3e49
이아름 <areum6321@gmail.com>
2024-08-12 20:02:04 +0900

    chore: import 및 코드 정리

--- src/main/java/com/wanted/repository/user/UserRepository.java
+++ src/main/java/com/wanted/repository/user/UserRepository.java
@@ -2,7 +2,6 @@ package com.wanted.repository.user;
 
 import com.wanted.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
