f93dc38217b450cc3e7e739f37f6f07de602acf2
이아름 <areum6321@gmail.com>
2024-08-12 20:02:53 +0900

    comment: 주석 추가

--- src/main/java/com/wanted/controller/JobPostingController.java
+++ src/main/java/com/wanted/controller/JobPostingController.java
@@ -19,34 +19,40 @@ public class JobPostingController {
 
     private final JobPostingService jobPostingService;
 
+    // 채용 공고 등록
     @PostMapping(value = "/job-posting")
     public BaseResponse create(@Validated @RequestBody JobPostingCreateRequest request) {
         jobPostingService.create(request);
         return new BaseResponse();
     }
 
+    // 채용 공고 수정
     @PutMapping(value = "/job-posting/{jobPostingId}")
     public BaseResponse update(@PathVariable Long jobPostingId, @Validated @RequestBody JobPostingUpdateRequest request) {
         jobPostingService.update(jobPostingId, request);
         return new BaseResponse();
     }
 
+    // 채용 공고 삭제
     @DeleteMapping(value = "/job-posting/{jobPostingId}")
     public BaseResponse delete(@PathVariable Long jobPostingId) {
         jobPostingService.delete(jobPostingId);
         return new BaseResponse();
     }
 
+    // 채용 공고 목록 확인
     @GetMapping(value = "/job-posting")
     public List<JobPostingResponse> getList(@RequestParam(required = false) String search) {
         return jobPostingService.getList(search);
     }
 
+    // 채용 공고 상세페이지 확인
     @GetMapping(value = "/job-posting/{jobPostingId}")
     public JobPostingDetailResponse getDetail(@PathVariable Long jobPostingId) {
         return jobPostingService.getDetail(jobPostingId);
     }
 
+    // 채용 공고 지원
     @PostMapping(value = "/job-posting/apply")
     public BaseResponse apply(@Validated @RequestBody JobPostingApplyRequest request) {
         jobPostingService.apply(request);
